1450e40045c14385bc9301ed9c72f2e92b1a9fa1
James <10740291+Electrocyte@users.noreply.github.com>
2022-09-13 09:00:41 +0800

    Pathfinding enemies

---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    [SerializeField] WaveConfigSO waveConfig;
+    List<Transform> waypoints;
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPath();
+    }
+
+    private void FollowPath()
+    {
+        if (waypointIndex < waypoints.Count) {
+            Vector3 targetPos = waypoints[waypointIndex].position;
+            float delta = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, delta);
+            if (transform.position == targetPos) {
+                waypointIndex++;
+            }
+        } else {
+            Destroy(gameObject);
+        }
+
+    }
+}
